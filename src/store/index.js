2d22e567063bd33af3ada5711cdd35f44820a967
Leon Rostek <leon.rostek.business@gmail.com>
2019-09-04 18:12:00 +0200

    last changes

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+import * as Cookies from 'js-cookie'
 
 import shared from './shared'
 import news from './news'
@@ -11,6 +13,17 @@ import playreports from './playreports'
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
+    plugins: [
+        createPersistedState({
+            storage: {
+                getItem: key => Cookies.get(key),
+                // Please see https://github.com/js-cookie/js-cookie#json, on how to handle JSON.
+                setItem: (key, value) =>
+                Cookies.set(key, value, { expires: 3, secure: false }),
+                removeItem: key => Cookies.remove(key),
+            },
+        }),
+    ],
     modules: {
         shared: shared,
         news: news,
